2b3ccc3f36e97f22bc59396e7c7e8f364fab8e16
VBence2008 <99590141+VBence2008@users.noreply.github.com>
2022-08-11 18:51:49 +0200

    Add files via upload

--- Naisrever.py
+++ Naisrever.py
@@ -1,23 +1,27 @@
-sentence = "This 'Naisrever' program reverses the order of words in a sentence, and some other things."
-# Now reverse the sentence
-sentence = sentence[::-1]
-# if the first letter is a mark, move it to the end
-if sentence[0] in [".", ",", "!", "?"]:
-    sentence = sentence[1:] + sentence[0]
-    # Lowercase only the second last letter
-    sentence = sentence[:-2] + sentence[-2].lower() + sentence[-1]
-    # else, lowercase only the last letter
-else:
-    sentence = sentence[:-1] + sentence[-1].lower()
-# capitalize the first letter
-sentence = sentence[0].upper() + sentence[1:]
-# replace the marks
-sentence = sentence.replace(" ,", ", ")
-# Now go through each word and if it contains two quotes, reverse the word
-words = sentence.split(" ")
-for i in range(len(words)):
-    if words[i].count("'") == 2:
-        words[i] = words[i][::-1]
-sentence = " ".join(words)
-sentence = sentence.replace("'", "")
-print(sentence)
+def translate(sentence):
+    sentence = "This 'Naisrever' program reverses the order of words in a sentence, and some other things."
+    # Now reverse the sentence
+    sentence = sentence[::-1]
+    # if the first letter is a mark, move it to the end
+    if sentence[0] in [".", ",", "!", "?"]:
+        sentence = sentence[1:] + sentence[0]
+        # Lowercase only the second last letter
+        sentence = sentence[:-2] + sentence[-2].lower() + sentence[-1]
+        # else, lowercase only the last letter
+    else:
+        sentence = sentence[:-1] + sentence[-1].lower()
+    # capitalize the first letter
+    sentence = sentence[0].upper() + sentence[1:]
+    # replace the marks
+    sentence = sentence.replace(" ,", ", ")
+    # Now go through each word and if it contains two quotes, reverse the word
+    words = sentence.split(" ")
+    for i in range(len(words)):
+        if words[i].count("'") == 2:
+            words[i] = words[i][::-1]
+    sentence = " ".join(words)
+    sentence = sentence.replace("'", "")
+    return sentence
+
+
+print(translate("This 'Naisrever' program reverses the order of words in a sentence, and some other things."))
